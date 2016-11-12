93c7a58f42c41ab49f8446fd96756607681077e2
Sam Wyman <sam.wyman@gmail.com>
2016-11-12 13:44:36 -0800

    mvp

--- shot.sh
+++ shot.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # shots shots shots
+if [ -z "$1" ]; then 
+	echo usage: $0 deckname
+	exit
+fi
 
 CROP_WIDTH=20
 CROP_HEIGHT=820
 CROP_SIZE=${CROP_WIDTHx}${CROP_HEIGHT}
 
+RAW_PATH=./tmp/raw.png
+OUT_PATH=./decks/$1.png
 
-screencapture -w ./raw.png
-gm convert -crop 220x800+1170+60 ./raw.png ./out.png
-open out.png
+
+screencapture -w $RAW_PATH
+gm convert -crop 220x800+1170+60 $RAW_PATH $OUT_PATH
+open $OUT_PATH
